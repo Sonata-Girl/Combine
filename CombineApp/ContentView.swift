f4f8d3c186ad11718c13d14a021508bb8a4a0c04
SonataGirl <Ino-sonata1988>
2024-05-21 02:15:52 +0300

    добавила Pipeline

--- CombineApp/ContentView.swift
+++ CombineApp/ContentView.swift
@@ -6,19 +6,46 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        FirstPipelineView()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct FirstPipelineView: View {
+
+    @StateObject var viewModel = FirstPipelineViewModel()
+
+    var body: some View {
+        HStack {
+            TextField("Ваше имя", text: $viewModel.name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text(viewModel.validation)
+        }
+        .padding()
+        Text("Hello, \(viewModel.name)")
+    }
+}
+
+class FirstPipelineViewModel: ObservableObject {
+    @Published var name = "..."
+    @Published var validation = ""
+
+    init() {
+        $name
+            .map { $0.isEmpty ? "X" : "V"}
+            .assign(to: &$validation)
+    }
+}
+
+struct FirstPipelineView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstPipelineView()
+    }
 }

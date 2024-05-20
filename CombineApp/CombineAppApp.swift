3fe1fe977caf241d53a886783fa8d4b68f5861ea
SonataGirl <Ino-sonata1988>
2024-05-20 21:09:13 +0300

    Initial Commit

---
+++ CombineApp/CombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAppApp.swift
+//  CombineApp
+//
+//  Created by Sonata Girl on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

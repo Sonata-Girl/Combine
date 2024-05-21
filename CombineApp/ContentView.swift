919120528538a47308e1dec1a6d54c0ddc8ccd22
SonataGirl <Ino-sonata1988>
2024-05-21 14:23:02 +0300

    отмена подписки имитация загрузки данных

--- CombineApp/ContentView.swift
+++ CombineApp/ContentView.swift
@@ -24,40 +24,71 @@ struct FirstPipelineView: View {
 
     var body: some View {
         VStack {
-            TextField("Ваше имя", text: $viewModel.name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField("Ваша фамилия", text: $viewModel.surname)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            Text(viewModel.validation)
+            Spacer()
+            Text(viewModel.data)
+                .font(.title)
+                .foregroundStyle(.green)
+            Text(viewModel.status)
+                .foregroundStyle(.blue)
+
+            Spacer()
+
+            Button {
+                viewModel.cancel()
+            } label: {
+                Text("Отменить подписку")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal)
+            }
+            .background(.red)
+            .cornerRadius(8)
+            .opacity(viewModel.status == "Запрос в банк..." ? 1.0 : 0.0)
+
+            Button {
+                viewModel.refresh()
+            } label: {
+                Text("Запрос данных")
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal)
+            }
+            .background(.blue)
+            .cornerRadius(8)
+            .padding()
         }
         .padding()
-        Text("Hello, \(viewModel.name) \(viewModel.surname)")
-        Button("Отмена подписки") {
-            viewModel.validation = ""
-            viewModel.anyCancellable?.cancel()
-        }
+
     }
 }
 
 final class FirstPipelineViewModel: ObservableObject {
-    @Published var name = "..."
-    @Published var surname = "..."
+    @Published var data = ""
+    @Published var status = ""
     @Published var validation = ""
 
-    var anyCancellable: AnyCancellable?
+    private var cancellable: AnyCancellable?
 
     init() {
-        anyCancellable = $name
-            .map { $0.isEmpty || self.surname.isEmpty ? "❌" : "✅"}
-            .sink { value in
-                self.validation = value
+        cancellable = $data
+            .map { [unowned self] value -> String in
+                self.status = "Запрос в банк..."
+                return value
+            }
+            .delay(for: 5, scheduler: DispatchQueue.main)
+            .sink { [unowned self] value in
+                self.data = "Сумма всех счетов 1 млн."
+                self.status = "Данные получены."
 
             }
-        anyCancellable = $surname
-            .map { $0.isEmpty || self.name.isEmpty ? "❌" : "✅"}
-            .sink { value in
-                self.validation = value
+    }
 
-            }
+    func refresh() {
+        data = "Перезапрос данных"
+    }
+
+    func cancel() {
+        status = "Операция отменена"
+        cancellable?.cancel()
+        cancellable = nil
     }
 }

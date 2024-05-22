c74ae6c2d2a16575f5f2ea1c974ff08bac1080cc
SonataGirl <Ino-sonata1988>
2024-05-23 00:35:30 +0300

    Сделано задание добавление в список

--- CombineApp/ContentView.swift
+++ CombineApp/ContentView.swift
@@ -10,7 +10,7 @@ import Combine
 
 struct ContentView: View {
     var body: some View {
-        GuessNumberView()
+        EmptyFailurePublishersView()
     }
 }
 
@@ -18,68 +18,95 @@ struct ContentView: View {
     ContentView()
 }
 
-struct GuessNumberView: View {
+struct EmptyFailurePublishersView: View {
 
-    @StateObject private var viewModel = GuessNumberViewModel()
-    @State private var showNumber = false
+    @StateObject private var viewModel = EmptyFailurePublishersViewModel()
+    @State var textFieldString: String = ""
 
     var body: some View {
         VStack {
-            Spacer()
-            TextField("Введите любое число", text: $viewModel.selection.value)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.numberPad)
-                .padding()
-            Button("Завершить игру") {
-                showNumber.toggle()
-                viewModel.cancelGame()
+            VStack {
+                Spacer()
+                TextField("Введите строку", text: $viewModel.inputText.value)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                HStack {
+                    if viewModel.isButtonVisible {
+                        Button("Добавить") {
+                            viewModel.selectionForAddWord.value = viewModel.inputText.value
+                        }
+                    } else {
+                        Text("Добавить")
+                            .foregroundStyle(.gray)
+                    }
+                    Button("Очистить список") {
+                       viewModel.clearList()
+                    }
+                }
             }
-            Spacer()
-            Text(viewModel.text.value)
-
-            if showNumber {
-                Text("Загаданное число: \(viewModel.guessNumber.value)")
+            .background(.white)
+            .frame(height: 150)
+            VStack {
+                List(viewModel.dataToView.value, id: \.self) { item in
+                    Text(item)
+                }
             }
+            .background(Color(uiColor: .lightGray))
+            Spacer()
         }
     }
 }
 
-final class GuessNumberViewModel: ObservableObject {
+final class EmptyFailurePublishersViewModel: ObservableObject {
+    var dataToView = CurrentValueSubject<[String], Never>([])
+    @Published var isButtonVisible = false
+    var selectionForAddWord = CurrentValueSubject<String, Never>("")
+    var inputText = CurrentValueSubject<String, Never>("")
 
-    var selection = CurrentValueSubject<String, Never>("0")
-    var guessNumber = CurrentValueSubject<Int, Never>(Int.random(in: 1...100))
-    var text = CurrentValueSubject<String, Never>("")
+    var datas: [String?] = []
     var cancellable: Set<AnyCancellable> = []
 
     init() {
-        selection
-            .map { [unowned self] newValue -> Int in
-                if Int(newValue) ?? 0 == guessNumber.value {
-                    return 1
-                } else if Int(newValue) ?? 0 > guessNumber.value {
-                    return 2
-                } else if Int(newValue) ?? 0 < guessNumber.value {
-                    return 3
-                } else {
-                    return 0
-                }
+        inputText
+            .map { newValue -> Bool in
+                newValue.isEmpty ? false : true
             }
-            .delay(for: 0.8, scheduler: DispatchQueue.main)
             .sink { [unowned self] value in
-                if value == 2 {
-                    text.value = "Введенное число больше загаданного"
-                } else if value == 3 {
-                    text.value = "Введенное число меньше загаданного"
-                } else if value == 1 {
-                    text.value = "Вы угадали!"
-                }
-                // запускаем обновление UI
-                objectWillChange.send()
+                self.isButtonVisible = value
+            }
+            .store(in: &cancellable)
+        selectionForAddWord
+            .filter {
+                !$0.isEmpty
+            }
+            .sink { [unowned self] newValue in
+                datas.append(newValue)
+                dataToView.value.removeAll()
+                fetch()
+                inputText.value = ""
             }
             .store(in: &cancellable)
     }
 
-    func cancelGame() {
-        cancellable.removeAll()
+    func fetch() {
+        _ = datas.publisher
+            .flatMap { item -> AnyPublisher<String, Never> in
+                if let item = item {
+                    return Just(item)
+                        .eraseToAnyPublisher()
+                } else {
+                    return Empty(completeImmediately: true)
+                        .eraseToAnyPublisher()
+                }
+            }
+            .sink { [unowned self] item in
+                dataToView.value.append(item)
+            }
+    }
+
+    func clearList() {
+        datas.removeAll()
+        dataToView.value.removeAll()
+        objectWillChange.send()
     }
 }

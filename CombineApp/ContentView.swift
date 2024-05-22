65634e787b47156af970eeaece5902c280222e58
SonataGirl <Ino-sonata1988>
2024-05-22 11:14:10 +0300

    Завершена игра угадай число

--- CombineApp/ContentView.swift
+++ CombineApp/ContentView.swift
@@ -35,13 +35,8 @@ struct GuessNumberView: View {
                 viewModel.cancelGame()
             }
             Spacer()
-            if viewModel.numberText.value == 2 {
-                Text("Введенное число больше загаданного")
-            } else if viewModel.numberText.value == 3 {
-                Text("Введенное число меньше загаданного")
-            } else if viewModel.numberText.value == 1 {
-                Text("Вы угадали!")
-            }
+            Text(viewModel.text.value)
+
             if showNumber {
                 Text("Загаданное число: \(viewModel.guessNumber.value)")
             }
@@ -53,7 +48,7 @@ final class GuessNumberViewModel: ObservableObject {
 
     var selection = CurrentValueSubject<String, Never>("0")
     var guessNumber = CurrentValueSubject<Int, Never>(Int.random(in: 1...100))
-    var numberText = CurrentValueSubject<Int, Never>(0)
+    var text = CurrentValueSubject<String, Never>("")
     var cancellable: Set<AnyCancellable> = []
 
     init() {
@@ -71,7 +66,13 @@ final class GuessNumberViewModel: ObservableObject {
             }
             .delay(for: 0.8, scheduler: DispatchQueue.main)
             .sink { [unowned self] value in
-                numberText.value = value
+                if value == 2 {
+                    text.value = "Введенное число больше загаданного"
+                } else if value == 3 {
+                    text.value = "Введенное число меньше загаданного"
+                } else if value == 1 {
+                    text.value = "Вы угадали!"
+                }
                 // запускаем обновление UI
                 objectWillChange.send()
             }
@@ -82,64 +83,3 @@ final class GuessNumberViewModel: ObservableObject {
         cancellable.removeAll()
     }
 }
-
-
-//PUBLISHED //////////////////
-
-//import SwiftUI
-//import Combine
-//
-//struct ContentView: View {
-//    var body: some View {
-//        CurrentValueSubjectView()
-//    }
-//}
-//
-//#Preview {
-//    ContentView()
-//}
-//
-//struct CurrentValueSubjectView: View {
-//
-//    @StateObject private var viewModel = CurrentValueSubjectViewModel()
-//
-//    var body: some View {
-//        VStack {
-//            Text("\(viewModel.selectionSame ? "Два раза выбрали" : "") \(viewModel.selection)")
-//                .foregroundStyle(viewModel.selectionSame ? .red : .green)
-//                .padding()
-//            Button("Выбрать колу") {
-//                viewModel.selection = "Кола"
-//            }
-//            .padding()
-//            Button("Выбрать бургер") {
-//                viewModel.selection = "Бургер"
-//            }
-//            .padding()
-//        }
-//    }
-//}
-//
-//final class CurrentValueSubjectViewModel: ObservableObject {
-//
-//    @Published var selection = "Корзина пуста"
-//    @Published var selectionSame = false
-//
-//    var cancellable: Set<AnyCancellable> = []
-//
-//    init() {
-//        $selection
-//            .map { [unowned self] newValue -> Bool in
-//                if newValue == selection {
-//                    return true
-//                } else {
-//                    return false
-//                }
-//            }
-//            .sink { [unowned self] value in
-//                print(value)
-//                selectionSame = value
-//            }
-//            .store(in: &cancellable)
-//    }
-//}
